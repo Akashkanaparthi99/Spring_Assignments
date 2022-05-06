c88130b63b7b106be5250231fcf29e2f406440c7
AkashKanaparthi <akashkanaparthi110@gmail.com>
2022-05-06 17:54:21 +0530

    Assignments

---
+++ spring-security-freelancejwt/src/main/java/com/freelanceapp/SpringSecurityFreelancejwtApplication.java
@@ -0,0 +1,13 @@
+package com.freelanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFreelancejwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFreelancejwtApplication.class, args);
+	}
+
+}
